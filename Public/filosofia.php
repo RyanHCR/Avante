20a178b224e6e4e2be8c83445a0a6a5e60fddc51
SaadRyan <r1227ryan2@gmail.com>
2024-09-13 08:30:08 -0300

    prototipo na brach teste

---
+++ Public/filosofia.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <header>
+        <a href="historia.php">HISTÓRIA</a>
+        <a href="geografia.php">GEOGRAFIA</a>
+        <img src="">
+        <a href="sociologia.php">SOCIOLOGIA</a>
+        <a href="filosofia.php">FILOSOFIA</a>
+    </header>
+<section>
+<a href="filosofia-antiga.php">FILOSOFIA ANTIGA</a>
+<a href="filosofia-medieval.php">FILOSOFIA MEDIEVAL</a>
+<a href="filosofia-moderna.php">FILOSOFIA MODERNA</a>
+<a href="filosofia-moderna.php">FILOSOFIA MODERNA</a>
+
+</section>
+</body>
+</html>
