7f9c8540adb3f1c520c83ceba44439f1d25647e9
FAB10-H3NRIQUE <fabiohpereira2007@hotmail.com>
2024-09-18 08:17:54 -0300

    ajuste de tela inicial

--- Public/materias/historia.php
+++ Public/materias/historia.php
@@ -3,7 +3,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Botões CRUD</title>
-    <link rel="stylesheet" href="Resources/css/index.css">
+    <link rel="stylesheet" href="../../Resources/css/index.css">
 </head>
 <body>
     
