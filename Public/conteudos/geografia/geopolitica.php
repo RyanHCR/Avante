236be73bbc9ee3b92eb5ba7a80a76d5e1a7dff1d
KarinaMauro <karinadasilvamauro@gmail.com>
2024-10-04 07:23:01 -0300

    pagina economia - geografia

--- Public/conteudos/geografia/geopolitica.php
+++ Public/conteudos/geografia/geopolitica.php
@@ -4,6 +4,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>GEOPOLÍTICA</title>
     <link rel="stylesheet" href="../../../Resources/css/index.css">
+    <link rel="stylesheet" href="../../../Resources/css/geografia.css/geopolitica.css">
 </head>
 <body>
     
@@ -21,39 +22,49 @@
     </div>
 </header>
 
-<img src="../../../Resources/images/imagem1.pngutton-back">VOLTAR</a>
-    <p>Geopolítica é uma área do conhecimento que se dedica ao estudo das relações de poder entre Estados e territórios, bem como a forma como se organizam no sistema-mundo.</p>
-    <p>A geopolítica é um campo de estudo interdisciplinar que examina como fatores geográficos, econômicos e políticos influenciam as relações de poder entre as nações. Esse conceito vai além da simples análise territorial, focando nas estratégias que os países adotam para manter sua soberania, garantir acesso a recursos e se posicionar no cenário internacional. Ao estudar geopolítica, entendemos os motivos por trás de conflitos, alianças e disputas por recursos que moldam o mundo atual.</p>
+<a href="../../materias/geografia.php" class="button-back">VOLTAR</a>
 
-    <p>A geopolítica é um campo de estudo interdisciplinar que examina como fatores geográficos, econômicos e políticos influenciam as relações de poder entre as nações. Esse conceito vai além da simples análise territorial, focando nas estratégias que os países adotam para manter sua soberania, garantir acesso a recursos e se posicionar no cenário internacional. Ao estudar geopolítica, entendemos os motivos por trás de conflitos, alianças e disputas por recursos que moldam o mundo atual.</p>
-    <img src="../../../Resources/images/geopolitica1.jpg">
+<div class="texto1">Geopolítica é uma área do conhecimento que se dedica ao estudo das relações de poder entre Estados e territórios, bem como a forma como se organizam no<br> sistema-mundo.</div>
+<div class="texto2">A geopolítica é um campo de estudo interdisciplinar que examina como fatores geográficos, econômicos e políticos influenciam as relações de poder entre as nações. Esse conceito vai além da simples análise territorial, focando nas estratégias que os países adotam para manter sua soberania, garantir acesso a recursos e se posicionar no cenário internacional. Ao estudar geopolítica, entendemos os motivos por trás de conflitos, alianças e disputas por recursos que moldam o mundo atual.</div>
 
-    <p>A ORIGEM DO CONCEITO</p>
-    <p>O termo "geopolítica" foi usado pela primeira vez pelo cientista político sueco Rudolf Kjellén, no início do século XX. Desde então, ele evoluiu para se tornar uma ferramenta essencial para analisar a dinâmica global. Inicialmente, a geopolítica estava muito ligada às ambições imperialistas dos grandes impérios, com foco na expansão territorial e controle de áreas estratégicas. Ao longo do tempo, o conceito se expandiu, incorporando novos elementos como globalização, tecnologia e crises ambientais.</p>
-    <img src="../../../Resources/images/geopolitica3.jpg">
+<div class="imagem1"><img src="../../../Resources/images/geopolitica1.jpg"></div>
+
+<div class="texto3">A ORIGEM DO CONCEITO</div>
+<div class="texto4">O termo "geopolítica" foi usado pela primeira vez pelo cientista político sueco Rudolf Kjellén, no início do século XX. Desde então, ele evoluiu para se tornar uma ferramenta essencial para analisar a dinâmica global. Inicialmente, a geopolítica estava muito ligada às ambições imperialistas dos grandes impérios, com foco na expansão territorial e controle de áreas estratégicas. Ao longo do tempo, o conceito se expandiu, incorporando novos elementos como globalização, tecnologia e crises ambientais.</div>
+<div class="imagem2"><img src="../../../Resources/images/geopolitica3.jpg"></div>
     
-    <p>FATORES GEOPOLÍTICOS</p>
-    <p>A geopolítica leva em conta vários fatores para analisar o comportamento dos Estados no cenário internacional:</p>
-    <li>Geografia: A localização física de um país influencia suas oportunidades e desafios. Um exemplo claro disso é a Rússia, que sempre buscou acesso a mares quentes para garantir uma saída naval viável durante todo o ano. Países insulares, como o Reino Unido e o Japão, historicamente se beneficiaram de sua proteção natural contra invasões terrestres.</li>
-    <li>Recursos Naturais: A presença de petróleo, gás natural, minérios ou água potável pode determinar o poder de um país. O Oriente Médio, por exemplo, é uma região chave na geopolítica global por abrigar as maiores reservas de petróleo do mundo, o que faz com que potências globais mantenham interesses estratégicos na área.</li>
-    <li>Demografia: O tamanho e a composição da população também influenciam o poder geopolítico. Nações com populações jovens, como a Índia, têm a vantagem de uma força de trabalho crescente, enquanto países com populações envelhecidas, como o Japão, enfrentam desafios para manter o crescimento econômico.</li>
+<div class="texto5">FATORES GEOPOLÍTICOS</div>
+<div class="texto6">A geopolítica leva em conta vários fatores para analisar o comportamento dos Estados no cenário internacional:<br><br>
+   &#9679;Geografia: A localização física de um país influencia suas oportunidades e desafios. Um exemplo claro disso é a Rússia, que sempre buscou acesso a mares quentes para garantir uma saída naval viável durante todo o ano. Países insulares, como o Reino Unido e o Japão, historicamente se beneficiaram de sua proteção natural contra invasões terrestres.<br><br>
+   &#9679;Recursos Naturais: A presença de petróleo, gás natural, minérios ou água potável pode determinar o poder de um país. O Oriente Médio, por exemplo, é uma região chave na geopolítica global por abrigar as maiores reservas de petróleo do mundo, o que faz com que potências globais mantenham interesses estratégicos na área.<br><br>
+   &#9679;Demografia: O tamanho e a composição da população também influenciam o poder geopolítico. Nações com populações jovens, como a Índia, têm a vantagem de uma força de trabalho crescente, enquanto países com populações envelhecidas, como o Japão, enfrentam desafios para manter o crescimento econômico.</div>
 
-    <p>GEOPOLÍTICA COMTEMPÔRANEA</p>
-    <p>Na atualidade, a geopolítica envolve muito mais do que a simples conquista de território. Com a globalização, surgiram novos elementos que redefinem as relações de poder no mundo:</p>
-    <li>Globalização e Economia: As economias estão cada vez mais interligadas. O que acontece em uma parte do mundo, como uma crise financeira ou uma pandemia, pode impactar diretamente outras regiões. A China, por exemplo, emergiu como uma superpotência global devido ao seu crescimento econômico rápido e ao seu papel como a "fábrica do mundo".</li>
-    <li>Tecnologia e Cibersegurança: A disputa pelo domínio tecnológico é uma nova fronteira da geopolítica. A capacidade de desenvolver e controlar tecnologias de ponta, como a inteligência artificial e as redes 5G, tornou-se uma questão estratégica para países como os Estados Unidos e China.</li>
-    <li>Mudanças Climáticas: O aquecimento global trouxe novas preocupações geopolíticas. Nações estão disputando o controle de áreas do Ártico que, com o derretimento do gelo, podem revelar novos recursos minerais e rotas marítimas. Além disso, o deslocamento de populações devido a desastres naturais cria novas tensões e desafios migratórios.</li>
+   <div class="texto7">GEOPOLÍTICA COMTEMPÔRANEA</div>
+   <div class="texto8">Na atualidade, a geopolítica envolve muito mais do que a simples conquista de território. Com a globalização, surgiram novos elementos que redefinem as relações de poder no mundo:<br><br>
+   &#9679;Globalização e Economia: As economias estão cada vez mais interligadas. O que acontece em uma parte do mundo, como uma crise financeira ou uma pandemia, pode impactar diretamente outras regiões. A China, por exemplo, emergiu como uma superpotência global devido ao seu crescimento econômico rápido e ao seu papel como a "fábrica do mundo".<br><br>
+   &#9679;Tecnologia e Cibersegurança: A disputa pelo domínio tecnológico é uma nova fronteira da geopolítica. A capacidade de desenvolver e controlar tecnologias de ponta, como a inteligência artificial e as redes 5G, tornou-se uma questão estratégica para países como os Estados Unidos e China.<br><br>
+   &#9679;Mudanças Climáticas: O aquecimento global trouxe novas preocupações geopolíticas. Nações estão disputando o controle de áreas do Ártico que, com o derretimento do gelo, podem revelar novos recursos minerais e rotas marítimas. Além disso, o deslocamento de populações devido a desastres naturais cria novas tensões e desafios migratórios.</div>
     
-    <p>ORGANIZAÇÕES INTERNACIONAIS E GEOPOLÍTICA</p>
-    <p>Outro aspecto crucial da geopolítica moderna são as organizações internacionais, como a ONU (Organização das Nações Unidas), a OTAN (Organização do Tratado do Atlântico Norte) e a OMC (Organização Mundial do Comércio). Essas entidades regulam ou mediam conflitos entre nações, estabelecem normas de comércio e ajudam a manter a paz em áreas de tensão. No entanto, as relações de poder dentro dessas organizações muitas vezes refletem a geopolítica, com países como os Estados Unidos, China e Rússia exercendo maior influência em algumas decisões.</p>
-    <img src="../../../Resources/images/geopolitica2.jpg">
+   <div class="texto9">ORGANIZAÇÕES INTERNACIONAIS E GEOPOLÍTICA</div>
+   <div class="texto10">Outro aspecto crucial da geopolítica moderna são as organizações internacionais, como a ONU (Organização das Nações Unidas), a OTAN (Organização do Tratado do Atlântico Norte) e a OMC (Organização Mundial do Comércio). Essas entidades regulam ou mediam conflitos entre nações, estabelecem normas de comércio e ajudam a manter a paz em áreas de tensão. No entanto, as relações de poder dentro dessas organizações muitas vezes refletem a geopolítica, com países como os Estados Unidos, China e Rússia exercendo maior influência em algumas decisões.</div>
+   <div class="imagem3"><img src="../../../Resources/images/geopolitica2.jpg"></div>
 
-    <p>CONFLITOS GEOPOLÍTICOS</p>
-    <p>Alguns dos principais conflitos geopolíticos da atualidade envolvem:</p>
-    <p>Conflito Rússia-Ucrânia: Desde 2014, com a anexação da Crimeia pela Rússia, as tensões na região se intensificaram. A localização estratégica da Ucrânia e seu papel como corredor de gás natural para a Europa fazem com que este conflito tenha impactos globais.</p>
-    <p>Disputas no Mar do Sul da China: A China reivindica grande parte do Mar do Sul da China, uma região rica em recursos naturais e de importância estratégica para o comércio global. Isso gera tensões com outros países do sudeste asiático e com os Estados Unidos.</p>
-    <p>Crise Migratória e Refugiados: Conflitos como a guerra na Síria e instabilidades políticas na Venezuela criaram crises migratórias, forçando países a lidar com questões humanitárias e segurança de fronteiras.</p>
-    <img src="../../../Resources/images/geopolitica4.jpg">
+   <div class="linha1">-</div> <!-- Linha colorida -->
+   <div class="texto11">CONFLITOS GEOPOLÍTICOS</div>
+    <div class="linha2">-</div> <!-- Linha colorida -->
+    <div class="texto12">Alguns dos principais conflitos geopolíticos da atualidade envolvem:</div>
+
+<div class="container">
+    <div class="numero1">1</div>
+    <div class="numero2">2</div>
+    <div class="numero3">3</div>
+</div>
+<div class="container2">
+<div class="tabela1">Conflito Rússia-Ucrânia: <br>Desde 2014, com a anexação da Crimeia pela Rússia, as tensões na região se intensificaram. A localização estratégica da Ucrânia e seu papel como corredor de gás natural para a Europa fazem com que este conflito tenha impactos globais.</div>
+<div class="tabela2">Disputas no Mar do Sul da China: A China reivindica grande parte do Mar do Sul da China, uma região rica em recursos naturais e de importância estratégica para o comércio global. Isso gera tensões com outros países do sudeste asiático e com os Estados Unidos.</div>
+<div class="tabela3">Crise Migratória e Refugiados: Conflitos como a guerra na Síria e instabilidades políticas na Venezuela criaram crises migratórias, forçando países a lidar com questões humanitárias e segurança de fronteiras.</div>
+</div>
+<div class="imagem4"><img src="../../../Resources/images/geopolitica4.jpg"></div>
     <p></p>
     <p></p>
     <p></p>
