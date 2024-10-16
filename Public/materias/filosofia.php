c702d750ee5419fc402acb8bac1ff51060a067e7
FAB10-H3NRIQUE <fabiohpereira2007@hotmail.com>
2024-10-16 07:31:15 -0300

    nome das paginas

--- Public/materias/filosofia.php
+++ Public/materias/filosofia.php
@@ -2,7 +2,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Botões CRUD</title>
+    <title>Conteúdos de filosofia</title>
     <link rel="stylesheet" href="../../Resources/css/index.css">
 </head>
 <body>
